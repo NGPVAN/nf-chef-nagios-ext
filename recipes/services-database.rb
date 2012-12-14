99759851ce296a3c47d1cb8b6237222bacebd80e
Graham Christensen <gchristensen@nationalfield.org>
2012-12-14 12:00:05 -0500

    Removing the check from webheads.

--- recipes/services-database.rb
+++ recipes/services-database.rb
@@ -30,18 +30,3 @@ template "/etc/nagios3/conf.d/services-chef-database-slaves.cfg" do
     notifies :restart, "service[nagios3]"
 end
 
-trailing_servers = search(:node, "chef_environment:#{node.chef_environment} AND role:database-trailing")
-template "/etc/nagios3/conf.d/services-chef-database-trailing.cfg" do
-    source "service-chef.erb"
-
-    mode "0755"
-
-    variables(
-        :nodes => trailing_servers,
-        :description => "MySQL Trail Connectivity",
-        :command => 'check_mysql',
-        :hostgroup => 'app-web-servers'
-    )
-
-    notifies :restart, "service[nagios3]"
-end
